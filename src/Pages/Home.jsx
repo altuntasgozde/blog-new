1b72314cb2c8385fd105b1f0bd957e199a2edf99
Gözde <altuntasgozde@gmail.com>
2024-04-28 13:34:13 +0300

    sorting

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,15 +1,18 @@
 import React from 'react'
 
-export const Home = ({entries}) => {
+export const Home = ({ entries }) => {
+  
     return (
         <div>
             <div className='container mx-auto px-24 flex items-center flex-col'>
-                {entries.map((entry,index) => 
-                <div key={index}>
-                <p>{entry.input}</p>
-                <p>{entry.textarea}</p>
-                    </div>
-                )}
+                <div>
+                    {entries.slice(0).reverse().map((entry, index) =>
+                        <div key={index} className='border border-gray-400 p-5 rounded-lg mt-5'>
+                            <p className='font-bold'>{entry.input}</p>
+                            <p>{entry.textarea}</p>
+                        </div>
+                    )}
+                </div>
             </div>
         </div>
     )
