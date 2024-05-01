e44719bc1ac94b14148c9b534db5ee64f36b238b
Gözde <altuntasgozde@gmail.com>
2024-05-01 23:46:46 +0300

    adding axios and strapi

--- src/App.jsx
+++ src/App.jsx
@@ -1,21 +1,36 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Home } from './Pages/Home';
 import { NewEntry } from './Pages/NewEntry';
 import { Route, Routes } from 'react-router-dom';
 import { NavbarComp } from './components/NavbarComp';
+import axios from 'axios';
 
 const App = () => {
+
+  const [entry, setEntry] = useState([])
   const [entries, setEntries] = useState([]);
 
-  const handleEntryAdded = (newEntry) => {
-    setEntries([...entries, newEntry]);
-  };
+  useEffect(() => {
+    axios.get("http://localhost:1337/api/posts").then(response => {
+      setEntry(response.data.data)
+      console.log(entry)
+
+    })
+  }, [])
+
+
+
+  // const handleEntryAdded = () => {
+  //   // setEntries([...entries, entry]);
+  //   setEntries([...entries, entry]);
+  //   console.log(entries)
+  // };
   return (
     <>
       <NavbarComp />
       <Routes>
-        <Route path="/" element={<Home entries={entries}/>} />
-        <Route path="/newentry" element={<NewEntry onEntryAdded={handleEntryAdded}  />} />
+        <Route path="/" element={<Home entries={entries} />} />
+        {/* <Route path="/newentry" element={<NewEntry onEntryAdded={handleEntryAdded} />} /> */}
       </Routes>
     </>
   );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -6,10 +6,12 @@ export const Home = ({ entries }) => {
         <div>
             <div className='container mx-auto px-24 flex items-center flex-col'>
                 <div>
-                    {entries.slice(0).reverse().map((entry, index) =>
+                    {/* {entries.slice(0).reverse().map((entry, index) => */}
+                    {entries.map((entry, index) =>
+
                         <div key={index} className='border border-gray-400 p-5 rounded-lg mt-5'>
-                            <p className='font-bold'>{entry.input}</p>
-                            <p>{entry.textarea}</p>
+                            <p className='font-bold'>{entry.attributes.title}</p>
+                            <p>{entry.attributes.body}</p>
                         </div>
                     )}
                 </div>
